083366b3d5febecf0b7b78f89d2da20d890b35b6
Dev-OriolCC <cesaroriolcortezz@gmail.com>
2023-09-15 18:55:41 -0500

    Created project and added expo modules

--- App.js
+++ App.js
@@ -1,11 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Constants from 'expo-constants';
+//import * as Linking from 'expo-linking';
+import * as ExpoShazamKit from 'expo-shazamkit';
+import { MatchedItem } from 'expo-shazamkit';
+
+console.log(Constants.systemFonts);
 
 export default function App() {
-  return (
+
+  const [searching, setSearching] = useState(false);
+  const [song, setSong] = useState<MatchedItem | null>(null);
+
+  const [counterEvent, setCounterEvent] = useState(0);
+
+  const counter = () => {
+    console.log("Listening ["+counterEvent+"]: ");  
+    setCounterEvent(counterEvent+1);
+  }
+
+  const startListing = async () => {
+
+    try {
+      if (song) {
+        setSong(null)
+      }
+      setSearching(true);
+      //const result = await ExpoShazamKit.startListening();
+      const result = "test";
+      if (result.length > 0) {
+        setSong(result[0]);
+      } else {
+        Alert.alert("No Match", "No songs found");
+      }
+
+    } catch (e) {
+      console.log("Exception: " + e)
+    }
+
+    setSearching(false);
+
+  }
+
+
+  return (  
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World! My-Music-App-2</Text>
+      <Button onPress={startListing} title="Start Listening" />
       <StatusBar style="auto" />
     </View>
   );
